6e08b4d7d207b6b9a3b87cf73bbfa3425109ad40
Michael <m@kaverin.ml>
2022-05-13 23:07:26 +0300

    - generateUrlAuthToken

--- src/Contracts/AuthenticatableViaUrl.php
+++ src/Contracts/AuthenticatableViaUrl.php
@@ -4,7 +4,10 @@ namespace UrlLogin\Contracts;
 
 use Illuminate\Contracts\Auth\Authenticatable;
 
+/** @see \UrlLogin\Traits\AuthenticatesViaUrl */
 interface AuthenticatableViaUrl extends Authenticatable
 {
     public function getAuthTokenHash(): string;
+
+    public function generateUrlAuthToken(): string;
 }
--- src/Traits/AuthenticatesViaUrl.php
+++ src/Traits/AuthenticatesViaUrl.php
@@ -5,6 +5,7 @@ namespace UrlLogin\Traits;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
 
+/** @see \UrlLogin\Contracts\AuthenticatableViaUrl */
 trait AuthenticatesViaUrl
 {
     public function initializeAuthenticatesViaUrl()
@@ -17,12 +18,14 @@ trait AuthenticatesViaUrl
         return $this->{config('url-login.model_parameters.auth_token_hash')};
     }
 
-    public function generateTgAuthToken()
+    public function generateUrlAuthToken(): string
     {
         $token = Str::random(config('url-login.auth_token_length'));
 
         $this->{config('url-login.model_parameters.auth_token_hash')} = Hash::make($token);
         $this->{config('url-login.model_parameters.auth_token_expire')} = now()->addMinutes(config('url-login.model_parameters.auth_token_lifetime'));
         $this->save();
+
+        return $token;
     }
 }
